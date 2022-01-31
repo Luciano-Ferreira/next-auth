9aed82938126b10bed491286f09a7124c1b73042
Luciano Ferreira <luciano2023silva@gmail.com>
2022-01-31 19:52:36 -0300

    validating permissions (server-side)

--- utils/withSSRAuth.ts
+++ utils/withSSRAuth.ts
@@ -1,12 +1,21 @@
 import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from 'next';
 import { destroyCookie, parseCookies } from 'nookies';
+import decode from 'jwt-decode';
 import { AuthTokenError } from '../services/errors/AuthTokenError';
+import { validateUserPermissions } from './validateUserPermissions';
 
-export function withSSRAuth<P>(fn: GetServerSideProps<P>): GetServerSideProps {
+type WithSSRAuthOptions = {
+  permissions?: string[];
+  roles?: string[];
+}
+
+export function withSSRAuth<P>(fn: GetServerSideProps<P>, options?: WithSSRAuthOptions): GetServerSideProps {
   return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<P>> => {
     const cookies = parseCookies(ctx);
 
-    if (!cookies['nextauth.token']) {
+    const token = cookies['nextauth.token'];
+
+    if (!token) {
       return {
         redirect: {
           destination: '/',
@@ -14,6 +23,30 @@ export function withSSRAuth<P>(fn: GetServerSideProps<P>): GetServerSideProps {
         }
       }
     }
+
+    if (options) {
+      const user = decode<{ permissions: string[], roles: string[] }>(token)
+
+      const { permissions, roles } = options;
+
+      console.log(user)
+
+      const userHasValidPermissions = validateUserPermissions({
+        user,
+        permissions,
+        roles
+      })
+
+      if (!userHasValidPermissions) {
+        return {
+          redirect: {
+            destination: '/dashboard',
+            permanent: false
+          }
+        }
+      }
+    }
+
     try {
       return await fn(ctx)
     } catch (err) {
